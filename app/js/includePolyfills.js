1a67ffde0ea8508f4e1d3fc81f3fe401e03422f3
a571301 <regis.legrand@worldline.com>
2014-08-21 14:42:59 +0200

    PW3 solution completed
    Conflicts:
    	app/index.html

--- app/js/includePolyfills.js
+++ app/js/includePolyfills.js
@@ -12,7 +12,29 @@
 			}
 		}
 
+
+		var includeFlexboxCallback= function(){
+
+			$(document).ready(
+				//TODO: here
+				function(){
+					//Include css fallback
+					if ($('html').hasClass('no-flexbox')){
+						$('<link/>')
+						.attr('rel','stylesheet')
+						.attr('href', 'css/noFlexbox.css')
+						.appendTo($('html > head'));
+					}
+
+				}
+
+			);
+
+		}
+
+
 		includePromisesPolyfill();
+		includeFlexboxCallback();
 
 
 	}
