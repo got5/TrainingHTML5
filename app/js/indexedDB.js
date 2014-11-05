9621f0962edae3976c0c91d64b69997897b8b76f
a571301 <regis.legrand@worldline.com>
2014-11-05 16:13:02 +0100

    indexedDB initial version modified

--- app/js/indexedDB.js
+++ app/js/indexedDB.js
@@ -1,142 +1,147 @@
-
 var indexedDbModule=
 (
-
 	function($){
 
-		var currentModule= {};
+		var module={
 
-		var dbInitialized= false;
-		var placesDB;
+			dbName: 'placesDB',
+			dbVersion: 1,
+			dbStores:[{name:'places', keyPath:'id'}],
+			dbInstance: undefined,
 
+			/* START :: DEFINE MAINONLINE INTERFACE FUNCTIONS */
 
-		var initDb= function(){
+			getPins: function(success, failure){
 
-			var createAndInsertData= function(data){
+				var that= this;
 
-				var promise= new Promise(function(resolve, reject){
-
-				    //Property which gurantee the unicity of the object
-				    var keyPath= 'id';
-
-				    //TODO: Create the object store called 'places' 
-				    // (placesDB is already instanciated at this time)
-				    // For your object store, remember to specify the keypass, ie the property
-				    // of your object which contains the primary key
-
-
-				    // TODO: Create an index on the 'author' property, this will give the possibility to find
-				    // places by author (no functionalities implemented for this in the PW)
-
-				    //Store the data 
-
-
-				    // TODO: wait end of the transaction before continue:
-				    // Use the transaction field on your object store to retrieve the current transaction
-				    // Use the oncomplete/onerror triggers on the transaction to resolve/reject your promise
-				});
-
-				return promise;
-			}
-
-			var deleteDB= function(){
-					//Delete db
-					return indexedDB.deleteDatabase('placesDB');
-			}
-
-
-			var promise= new Promise(function(resolve, reject){
-
-				if (!dbInitialized){
-					mainOnline.getPins(
-						function(data){
-
-							var deleteRequest= deleteDB();
-							deleteRequest.onsuccess= function(event){
-
-								console.log('DB deleted');
-
-								var createRequest= indexedDB.open("placesDB");
-								createRequest.onsuccess= function(event){
-									placesDB= createRequest.result;
-									dbInitialized= true;
-									resolve('dbready');		
-								}
-								createRequest.onerror= function(e){
-									console.log('Error while creating/opening DB: ' + e);
-									reject(createRequest.error);
-								}
-								createRequest.onupgradeneeded= function(){
-									placesDB= createRequest.result;
-									createAndInsertData(data)
-									.then(function(successData){
-										dbInitialized= true;
-										resolve('dbready');
-									}, function(failureData){
-										reject(failureData);
-									});
-								}
-
-							}
-							deleteRequest.onerror= function(event){
-								console.log('An error occured while deleting DB');
-								console.log(event);
-								reject(deleteRequest.error);
-							}	
-						}
-					);
+				//If the DB is already initialized, 
+				// we just retrieve data from it
+				if (that.dbInstance){
+					that.getDataFromDb()
+					.then(success, failure);
 				}
+				//First time, we create it
 				else{
-					resolve('dbready');
-				}			
-			});
+					this.deleteDB()
+					.then(function(e){return that.createEmptyDB(e);})
+					.then(function(e){return that.populateDB(e);})
+					.then(function(e){return that.getDataFromDb(e);})
+					.then(function(places){success(places);})
+					.catch(function(error){
+						that.logError(error);
+						failure();
+					});
+				}
+			},
 
-			return promise;
-		}
+			/**
+			* Update DB content based on the place parameter.
+			* Then call the updatedSuccess callback.
+			*/
+			updateContent: function(place,updatedSuccess){
+				var transaction= this.dbInstance.transaction([this.dbStores[0].name], 'readwrite');
+				transaction.onerror= this.logError;
+				//oncomplete is called when the transaction is auto commit
+				transaction.oncomplete= updatedSuccess;
+				//TODO: replace the place in DB with the one in parameter
+
+				//TODO: Be sure to manage errors
+
+			},
+
+			/* END :: REPRODUCE MAINONLINE INTERFACE FUNCTIONS */
 
 
-		currentModule.getPins= function(successCallback, failureCallback){
+			/* START :: UTILITIES */
 
-			var retrieveData= function(){
+			logError: function(e){
+				console.log('An error occured');
+				if (e.target && e.target.errorCode){
+					console.log('errorCode = ' + e.target.errorCode);
+				}else{
+					console.log(e);
+				}
+			},
 
-				//TODO: Create an array in which let's add places
-				
+			/**
+			* Delete the database
+			*/
+			deleteDB: function(){
+				//TIP: Use that instead of this everywhere in the function
+				var that= this;
+				return new Promise(function(resolve, reject){
+					//TODO: delete the database (its name is in the fields)
 
-				//TODO: Create a new transaction which will hold the 'places' table
+					//TODO: Be sure to resolve the promise in case of success
+					//      And to reject it otherwise (take into accounts all
+					//      possible callbacks)
+				});
+			},
 
+			/**
+			* Create an empty database.
+			* Also defines the DB model (creates objectStores)
+			*/
+			createEmptyDB: function(event){
+				//TIP: Use that instead of this everywhere in the function
+				var that= this;
+				return new Promise(function(resolve, reject){
+					var openDbRequest= indexedDB.open(that.dbName, that.dbVersion);
 
-				//TODO: Add a callback to the transaction object to call the 'failureCallback' in parameter
-				// in case of failure (remember that bubbling events will make this error available
-				// at request level).
-				
+					//TODO: Handle 'error' and 'blocked' event to reject
+					//      the promise in that case 
 
-				//TODO: Get the 'places' objectstore through the transaction object
-				
+					openDbRequest.onsuccess= function(e){
+						// TODO: Initialize the dbInstance field and resolve the promise
+					}
+					openDbRequest.onupgradeneeded= function(e){
+						//TODO: Create object stores, don't forget the keyPath
+					}
+				});
+			},
 
-				//TODO: open a cursor on the 'places' objectStore to iterate
-				// over this table and add each place on the array created at begining
-				//Be sure to call the successCallback in parameter when iteration complete
+			populateDB: function(event){
+				//TIP: Use that instead of this everywhere in the function
+				var that= this;
+				return new Promise(function(resolve, reject){
+					mainOnline.getPins(
+						function(places){
+							var transaction= that.dbInstance.transaction([that.dbStores[0].name], 'readwrite');
+							transaction.onerror=reject;
+							//TODO: resolve the promise when the transaction is commit 
+							//      (look for updateContent() to get a sample)
 
+							//TODO: Get the objectstore and add the data in parameter.
+							//      In case of error on adding an object, don't reject
+							//      the promise but log the error instead
+							
+						}, reject
+					);
+				});
+			},
+
+			getDataFromDb: function(e){
+				//TIP: Use that instead of this everywhere in the function
+				var that= this;
+				return new Promise(function(resolve, reject){
+					var res=[];
+					var transaction= that.dbInstance.transaction([that.dbStores[0].name], 'readonly');
+					transaction.onerror= reject;
+					//oncomplete is called when the transaction is auto commit
+					transaction.oncomplete= function(e){resolve(res)};
+					var placesStore= transaction.objectStore(that.dbStores[0].name);
+
+					//TODO: Create a cursor to retrieve all objects in the store. 
+					//      Append these data in the 'res' array					
+				});
 			}
 
-			//TODO: Call the initDB function, and use promises mechanism
-			// to call the retrieveData function in case of success and
-			// log an error message in the console in case of failure
-			
+			/* END :: UTILITIES */
 		}
 
-		currentModule.updateContent= function(place, updatedSuccess){
-			//TODO: Open a readwrite transaction on the DB
-
-			//TODO: Get the 'places' objectStore 
-
-			//TODO: Update it with the current place
-
-			//Assign the 'updatedSuccess' parameter callback in case
-			// the request completes successfully
-		}
-
-		return currentModule;
+		//Then return the module
+		return module;
 	}
 
 )(jQuery)
